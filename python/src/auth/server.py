c0a30617408e0709efd3dd4b40d6e6d1e8b74840
dawid mrotek <DawidMrotek@protonmail.ch>
2023-06-09 18:56:28 +0200

    connect flask to mysql

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,3 +1,16 @@
 import jwt, datetime, os
 from flask import Flask, request
 from flask_mysqldb import MySQL
+
+# create server as a Flask object
+server = Flask(__name__)
+
+# config for mysql connection
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+# connect Flask to MySQL database
+mysql = MySQL(server)
