8b4645807a315e6603ef9d59154db3cf0613aed7
dawid mrotek <DawidMrotek@protonmail.ch>
2023-06-10 17:35:38 +0200

    [FIX] make email unique

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -15,7 +15,7 @@ USE auth;
 /* create table for users containing id, email and password */
 CREATE TABLE user (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQE,
+	email VARCHAR(255) NOT NULL UNIQUE,
 	password VARCHAR(255) NOT NULL
 );
 
