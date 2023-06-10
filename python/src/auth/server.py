69aca2007dde23a5493bbd56b05c4e9e251df7ca
dawid mrotek <DawidMrotek@protonmail.ch>
2023-06-10 21:34:57 +0200

    [ADD] check credentials

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -17,17 +17,35 @@ server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 mysql = MySQL(server)
 
 
+
+
 # create login route
 @server.route("/login", methods =["POST"])
 def login():
-    # get user data from authorization header
+    # get user data from basic authentication scheme
     auth = request.authorization
     if not auth:
         return "missing credentials", 401
     
     # check database for login and password where email matches username that came in a request   
     cur = mysql.connection.cursor()
-    res = cur.execute(
-        
+    res = cur.execute( 
         "SELECT email, password FROM user WHERE email=%s, (auth.username,)
     )
+    
+    # if user exists in database
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+        
+        # check if username and password match the ones from request 
+        if auth.username != email or auth.password != password:
+            return "invalid credentials", 401
+        else:
+            # return JWT using username and secret from virtual env
+            # todo
+            #return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    # if such user does not exists, then credentials are invalid
+    else:
+        return "invalid credentials", 401
