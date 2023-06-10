62ee6ae8960d076084be45f20398c638d7e41260
dawid mrotek <DawidMrotek@protonmail.ch>
2023-06-10 17:01:25 +0200

    [FIX] typo

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -14,7 +14,7 @@ USE auth;
 
 /* create table for users containing id, email and password */
 CREATE TABLE user (
-	id INT NOT NULL AUTO_INCRIMENT PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL,
 	password VARCHAR(255) NOT NULL
 );
