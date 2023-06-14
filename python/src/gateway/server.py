7c3986090ac663804130a35e937ea51a0f895be8
DawMro <dawidmrotek@protonmail.ch>
2023-06-14 14:10:59 -0700

    [ADD] finish login route

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -31,3 +31,9 @@ channel = connection.channel()
 @server.route("/login", methods =["POST"])
 def login():
 	token, err = access.login(request)
+	
+	if not err:
+		return token
+	else:
+		return err
+		
